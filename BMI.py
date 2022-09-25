754d0146159080aa41489a74c641e91957d49211
JAMES_TENACITY <112569058+JAMES22308@users.noreply.github.com>
2022-09-25 21:05:34 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,20 +1,30 @@
 import math
+import pyttsx3 
+jarvis = pyttsx3.init()
+rate = -5000
+jarvis.setProperty('rate', rate)
+name = input("name: ")
 height = float(input("Enter your height in cm: "))
 weight = float(input("Enter your weight in kg: "))
-
 bmi = weight / (height/100)**2
-
 print(f"You BMI  is {bmi}")
-
 if bmi <= 18.4:
-    print("underweight.")
+    print(f"underweight.{name}")
+    print(name)
 elif bmi <= 24.9:
-    print("Yieee hilti🤣.")
+    print(name)
+    print(f"yiee healthy si {name}")
+    jarvis.say(f"yiee healthy si {name}")
 elif bmi <= 29.9:
-    print("You are over weight.")
+    print(name)
+    print(f"You are over weight. {name}")
 elif bmi <= 34.9:
-    print("You are severely over weight.")
+    print(name)
+    print(f"You are severely over weight. {name}")
 elif bmi <= 39.9:
-    print("You are obese.")
+    print(name)
+    print(f"You are obese.{name}")
 else:
-    print(" you're literally unhealthy, obese.")
+    print(name)
+    print(f" you're literally unhealthy, obese.{name}")
+jarvis.runAndWait()
